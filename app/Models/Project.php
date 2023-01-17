1da33bc33f149e2e5648f6ef3b7e3244101049f3
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-01-17 17:54:02 -0300

    Establishing relationship between models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,24 @@ class Project extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(ProjectStatus::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,9 @@ class Task extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
